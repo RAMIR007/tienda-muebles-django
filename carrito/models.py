2b53ca38249610a8b06a1cac5ca9dbc611349e2b
RAMIR007 <ramiroxavier07@gmail.com>
2025-09-04 12:54:26 -0400

    creacion de modelos, vistas y pantillas

--- carrito/models.py
+++ carrito/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from catalogo.models import Producto
 
-# Create your models here.
+class Carrito(models.Model):
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True, blank=True
+    )
+    creado = models.DateTimeField(auto_now_add=True)
+
+    def total(self):
+        return sum(item.subtotal() for item in self.items.all())
+
+    def __str__(self):
+        return f"Carrito #{self.id} - {self.usuario or 'Anónimo'}"
+
+class ItemCarrito(models.Model):
+    carrito = models.ForeignKey(Carrito, related_name="items", on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def subtotal(self):
+        return self.producto.precio * self.cantidad
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.producto.nombre}"
+    
